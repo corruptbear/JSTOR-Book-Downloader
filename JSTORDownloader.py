5b4aa7dbde39841b2027c35eb52de89369af99a8
lws <wawnx716@gmail.com>
2022-11-02 19:42:11 -0700

    try undetected chromedriver

--- JSTORDownloader.py
+++ JSTORDownloader.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+import undetected_chromedriver as uc
 
 
 import os
@@ -18,6 +19,9 @@ import random
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
+import tempfile
+from functools import reduce
+import json
 
 
 #modify book address here, you can use the ones here to test the script
@@ -35,6 +39,44 @@ time_for_recaptcha = 100
 
 #To handle "Terms and Conditions" Agreement
 terms_accepted = False
+
+class ChromeWithPrefs(uc.Chrome):
+    def __init__(self, *args, options=None, **kwargs):
+        if options:
+            self._handle_prefs(options)
+
+        super().__init__(*args, options=options, **kwargs)
+
+        # remove the user_data_dir when quitting
+        self.keep_user_data_dir = False
+
+    @staticmethod
+    def _handle_prefs(options):
+        prefs = options.experimental_options.get("prefs")
+        if prefs:
+            # turn a (dotted key, value) into a proper nested dict
+            undot_prefs = {}
+            for key, value in prefs.items():
+                main_key, sub_key = key.split('.',1)
+                if main_key not in undot_prefs:
+                    undot_prefs[main_key]={}
+                undot_prefs[main_key][sub_key] = value              
+
+            # create an user_data_dir and add its path to the options
+            user_data_dir = os.path.normpath(tempfile.mkdtemp())
+            options.add_argument(f"--user-data-dir={user_data_dir}")
+
+            # create the preferences json file in its default directory
+            default_dir = os.path.join(user_data_dir, "Default")
+            os.mkdir(default_dir)
+
+            prefs_file = os.path.join(default_dir, "Preferences")
+            with open(prefs_file, encoding="latin1", mode="w") as f:
+                json.dump(undot_prefs, f)
+
+            # pylint: disable=protected-access
+            # remove the experimental_options to avoid an error
+            del options._experimental_options["prefs"]
 
 def accept_cookie():
     
@@ -81,15 +123,13 @@ directory = parent_directory + "New Folder " + datetime.now().strftime('%Y-%m-%d
 os.mkdir(directory)
 
 #setting up the webdriver
-chrome_options = webdriver.ChromeOptions()
 prefs = {"plugins.always_open_pdf_externally": True,"download.default_directory":directory,"download.prompt_for_download":False,"download.directory_upgrade":True,"safebrowsing.enabled":True,"download.extensions_to_open": "applications/pdf"}
-chrome_options.add_experimental_option("prefs",prefs)
-chrome_options.add_argument( '--disable-blink-features=AutomationControlled' )
+options = uc.ChromeOptions()
+options.add_experimental_option("prefs", prefs)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+# use the derived Chrome class that handles prefs
+driver = ChromeWithPrefs(options=options)
 
-#fake user agent
-driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'})
 print(driver.execute_script("return navigator.userAgent;"))
 
 
@@ -149,12 +189,17 @@ for i in range(num_chapters):
     #reduce download speed to avoid trigger captcha too frequently
     sleep(1+random.random())
 
+
+
 files_to_merge = [] 
 new_directory_name = parent_directory + book_title
+
 #rename the chapters
 for i in range(num_chapters):
     chapter_title_text = chapter_title_texts[i]
     current_name = directory + book_url.split('/')[-1] + '.'+str(i+1)+'.pdf'
+
+
     print('rename:',current_name,' to:',chapter_title_text)
     while os.path.exists(current_name)==False:
         pass
